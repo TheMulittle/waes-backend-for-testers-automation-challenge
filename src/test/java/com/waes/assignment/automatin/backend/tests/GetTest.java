1a87969f0b32a127630015ef2ca0cab64c9eb961
Murilo Gabardo Kramar <murilogabardo@hotmail.com>
2018-11-18 10:09:57 -0200

    First working version of Serenity + RestAssured

---
+++ src/test/java/com/waes/assignment/automatin/backend/tests/GetTest.java
@@ -0,0 +1,11 @@
+package com.waes.assignment.automatin.backend.tests;
+
+import com.waes.assignment.automatin.backend.TestBase;
+import org.junit.Test;
+
+public class GetTest extends TestBase {
+    @Test
+    public void test() {
+
+    }
+}
